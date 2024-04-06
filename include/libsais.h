38f39d24a32878b7303cccdd798ef3d5e9b07f6a
Ilya Grebnov <ilya.grebnov@gmail.com>
2024-04-05 20:33:45 -0700

    Version 2.8.1 Fixed out-of-bound memory access issue for large inputs. (#22)

--- include/libsais.h
+++ include/libsais.h
@@ -26,8 +26,8 @@ Please see the file LICENSE for full copyright information.
 
 #define LIBSAIS_VERSION_MAJOR   2
 #define LIBSAIS_VERSION_MINOR   8
-#define LIBSAIS_VERSION_PATCH   0
-#define LIBSAIS_VERSION_STRING  "2.8.0"
+#define LIBSAIS_VERSION_PATCH   1
+#define LIBSAIS_VERSION_STRING  "2.8.1"
 
 #ifdef _WIN32
     #ifdef LIBSAIS_SHARED
